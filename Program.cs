06138a7c45f784cd86676fb6ff3a569c8b2af3d4
“mayuri” <mayurisalunkhe01@gmail.com>
2021-10-25 13:09:02 +0530

    Adding welcome msg to main

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day14_LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Data Structure LinkedList!");
+        }
+    }
+}
